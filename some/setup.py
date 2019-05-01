3f83cb63ebdd16fbe32d0175fc3dadd2373f6666
computermouth <computermouth@crunchbangplusplus.org>
2019-05-01 15:20:45 -0700

    Initial

---
+++ some/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='obapps',
+      version='0.1.7',
+      description='Openbox Applications Editor',
+      url='http://obapps.sourceforge.net',
+      license='MIT',
+      author='Eric Bohlman',
+      author_email='ericbohlman@gmail.com',
+      scripts=['obapps'],
+      py_modules=['obaxutils']
+    )
